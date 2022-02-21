6707d8ef3f4c46208a4e762e78488a22eeb88dc2
EvgenyAkhmerov <akhmerov.ep@phystech.edu>
2022-02-21 21:57:12 +0300

    lesson_4 passed

---
+++ Programs/lesson_4/module.py
@@ -0,0 +1,10 @@
+def mul_nums(num_1, num_2):
+    """
+    this function does multiply two numbers
+    """
+    print(__name__)
+    return num_1 * num_2
+
+
+if __name__ == '__main__':
+    print('module, hello')
